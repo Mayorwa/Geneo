2610700e79b2e07d3e951046ff9c68e1c54486c6
Mayorwa <mayorviper.mo@gmail.com>
2026-06-30 23:46:24 +0100

    file changes

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -3,8 +3,8 @@ import Logo from "@/assets/img/logo.svg";
 const Header = () => {
     return (
         <div className="container my-4 flex items-center">
-            <img alt="Logo" src={Logo} className="h-9 block logo"/>
-             {/*<h4 className="text-base ml-4">Genea</h4>*/}
+            <img alt="Logo" src={Logo} className="h-10 block logo"/>
+             <h4 className="text-lg ml-2"><span>|</span> Herit</h4>
         </div>
     )
 }
